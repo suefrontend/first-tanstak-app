b796ed7e82e1a8c482784ee357a2a339c64688fd
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-03-20 05:22:19 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -9,38 +9,33 @@ function App() {
 
   const [term, setTerm] = useState("");
 
-  // const filteredUsers = data?.filter((user) => {
-  //   if (term !== "") {
-  //     return (
-  //       user.name.first.toLowerCase().includes(term.toLocaleLowerCase()) ||
-  //       user.name.last.toLowerCase().includes(term.toLowerCase())
-  //     );
-  //   }
-  // });
+  const filteredUsers = data?.filter((user) => {
+    if (term !== "") {
+      return (
+        user.name.first.toLowerCase().includes(term.toLocaleLowerCase()) ||
+        user.name.last.toLowerCase().includes(term.toLowerCase())
+      );
+    }
+  });
 
-  // console.log("term", term);
-  // console.log("filteredUsers", filteredUsers);
-  // console.log("data", data);
-
-  let content;
-  if (isLoading) return "Loading...";
-  if (isError) return `Error: ${error}`;
+  console.log("term", term);
+  console.log("filteredUsers", filteredUsers);
+  console.log("data", data);
 
   return (
     <div className="container mx-auto">
-      {content}
       <h1 className="text-2xl font-bold text-center py-8">
         My First Tanstak Query App - User Name Filter
       </h1>
       <Search term={term} setTerm={setTerm} />
-      {/* <UserList
+      <UserList
         usersData={
           !filteredUsers || (filteredUsers.length === 0 && term === "")
             ? data
             : filteredUsers
-        } */}
-      {/* /> */}
-      <UserList usersData={data} />
+        }
+      />
+      {/* <UserList usersData={data} /> */}
     </div>
   );
 }
